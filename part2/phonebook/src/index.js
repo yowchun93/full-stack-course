a5cedfc6a15d4edf93f174e33c161f3fceb3336f
yowchun93 <yowchun93@gmail.com>
2020-08-14 17:14:43 +0800

    get person with hooks

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './index.css';
+
+// npx json-server --port 3001 --watch db.json
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -10,28 +13,51 @@ const App = () => {
   // this is used for controller form input element
   const [newName, setNewName] = useState('')
 
-  const handleNewName = () => {
-    
+  const handleNewName = (event) => {
+    setNewName(event.target.value);
   }
+
+  const addNewName = (event) => {
+    event.preventDefault();
+    const newPerson = {name: newName}
+    setPersons(persons.concat(newPerson))
+  }
+
+  // use effect hook to get all the names
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(response => {
+      setPersons(response.data)
+    })
+  })
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNewName}>
         <div>
-          name: 
+          name:
           <input
-            value={newName} 
+            value={newName}
             onChange={handleNewName}
           >
           </input>
         </div>
         <div>
-          <button type="submit">add</button>
+          <button
+            type="submit"
+          >
+            add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => {
+        return(
+          <ul>
+            <li>{person.name}</li>
+          </ul>
+        )
+      })}
     </div>
   )
 }
